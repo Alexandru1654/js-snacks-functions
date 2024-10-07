2ee0ecd0aaf5f5d338ea5e31e081f2853f2ca9e3
alexandru233847 <alexutualexino33@gmail.com>
2024-10-07 18:09:45 +0200

    Function declaration

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,10 +4,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
-
+function filterByInitial(names, letter) {
+    return names.filter(name => {
+        if (typeof name === 'string') {
+            return name.charAt(0).toLowerCase() === (letter.toLocaleLowerCase())
+        } else {
+            return false;
+        }
+    }
+    )
+}
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const filteredNames = filterByInitial(names, 'A');
+console.log(filteredNames);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
