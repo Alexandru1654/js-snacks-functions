20d6a456c46bd53b96c68d7c4eb26719ec6a15da
alexandru233847 <alexutualexino33@gmail.com>
2024-10-07 17:20:38 +0200

    Function Array names

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -1,13 +1,19 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
-
-const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-
+function getInitials(names) {
+    return names.map(name => {
+        if (typeof name === 'string') {
+            return name.charAt(0);
+        } else {
+            return '';
+        }
+    })
+}
 
 // Dichiara la funzione qui.
-
+const names = ['Anna', 'Luca', 'Marco', 'Adele', 'Giovanni', 'Alessandra']
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const initials = getInitials(names);
+console.log(initials);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
