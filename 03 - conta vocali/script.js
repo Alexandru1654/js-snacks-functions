7b370046922616e1f73e7a393e62a9724ba8a71e
alexandru233847 <alexutualexino33@gmail.com>
2024-10-07 18:42:07 +0200

    Console result

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,13 +1,21 @@
-/* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
-
 const word = 'javascript';
 
+// Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno
+function contaVocali(stringa) {
+    const vocali = 'aeiou';
+    let conteggio = 0;
 
-// Dichiara la funzione qui.
-
+    // Dichiara la funzione qui.
+    for (let i = 0; i < stringa.length; i++) {
+        if (vocali.includes(stringa[i].toLowerCase())) {
+            conteggio++;
+        }
+    }
+    
+    return conteggio;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+const risultato = contaVocali(word);
+console.log(risultato); // Risultato atteso se si passa 'javascript': 3 (a, a, i)
 
-
-
-//Risultato atteso se si passa 'javascript': 3 (a, a, i)
